5f294c6595b48653a49c7d97d4fcc1b0642fdb02
jolrv <86866255+jolrv@users.noreply.github.com>
2022-07-18 15:39:21 +0900

    Update psnr.py

--- psnr.py
+++ psnr.py
@@ -3,18 +3,18 @@ import math
 import cv2
 
 original=cv2.imread("Kindred.png")
-contrast=cv2.imread("low_newskin.bmp")
+compressed=cv2.imread("low_newskin.bmp")
 
-def psnr(img1, img2):
-    mse=np.mean((img1-img2)**2)     #mean square error
-    print("MSE :", mse)
+def PSNR(original, compressed):
+    mse=np.mean((original-compressed)**2)     #mean square error
 
     if mse==0:
         return 100
 
-    PIXEL_MAX=255.0
+    max_pixel=255.0
+    psnr=20*math.log10(max_pixel/math.sqrt(mse))
 
-    return 20*math.log10(PIXEL_MAX/math.sqrt(mse))
+    return psnr
 
-result=psnr(original, contrast)
-print("PSNR :", result)
+result=PSNR(original, compressed)
+print("PSNR :", result, "db")
